0479356851a25738ac5fbfd2627d8e2166a61cc2
joelsantos-js <joeltere9@gmail.com>
2024-01-02 11:22:41 -0300

    Initial commit

---
+++ src/main/java/com/joel/br/Ds4/Ds2024Application.java
@@ -0,0 +1,13 @@
+package com.joel.br.Ds4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/Ds4/Ds2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.Ds4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
